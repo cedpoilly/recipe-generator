e64eb531574d0fe7345cefca723928421a47aafc
Cedric <cedpoilly@gmail.com>
2023-07-29 16:13:52 +0400

    logs

--- server/api/chat.post.ts
+++ server/api/chat.post.ts
@@ -3,6 +3,8 @@ import { getChatStream } from "../utils/ai"
 export default defineEventHandler(async (event) => {
   const { messages } = await readBody(event)
 
+  console.log("receieved the messages", messages)
+
   const stream = await getChatStream({ messages })
   console.log(stream)
 
